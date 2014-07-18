7a319244713dd0daf0f6e8ef53ea84a6008b6b73
David Lu <davidlu@Davids-Mac.local>
2014-07-17 23:11:04 -0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by David Lu on 2014-07-17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
